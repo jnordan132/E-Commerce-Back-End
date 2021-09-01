88255a71882650fb3ce9fdd61b51cdfae216367c
Jacob Nordan <jacob.nordan113322@gmail.com>
2021-09-01 14:34:27 -0400

    fixed wrong model in get all route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,7 +7,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
     try {
         const productData = await Product.findAll({
-            include: [{ model: Tag }],
+            include: [{ model: Category }],
         });
         res.status(200).json(productData);
     } catch (err) {
