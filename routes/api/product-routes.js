d9bbb59bc298b189d5b2008fc0a39362cbdba571
Jacob Nordan <jacob.nordan113322@gmail.com>
2021-09-01 14:36:24 -0400

    corrected typo in get by id route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -17,21 +17,19 @@ router.get('/', (req, res) => {
 
 // get specific product based on id
 router.get('/:id', (req, res) => {
-    router.get('/:id', async(req, res) => {
-        try {
-            const productData = await Product.findByPk(req.params.id, {
-                include: [{ model: Reader }],
-            });
+    try {
+        const productData = await Product.findByPk(req.params.id, {
+            include: [{ model: Reader }],
+        });
 
-            if (!productData) {
-                res.status(404).json({ message: 'No product found with that id!' });
-                return;
-            }
-            res.status(200).json(productData);
-        } catch (err) {
-            res.status(500).json(err);
+        if (!productData) {
+            res.status(404).json({ message: 'No product found with that id!' });
+            return;
         }
-    });
+        res.status(200).json(productData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 // create new product
